759f68422c5e22ab034868f926978b5cf1aa022b
RukeshB <rukeshbasukala@gmail.com>
2021-07-19 17:27:28 +0545

    validation added

--- src/main/java/com/finalproject/HRM/entities/User.java
+++ src/main/java/com/finalproject/HRM/entities/User.java
@@ -2,6 +2,10 @@ package com.finalproject.HRM.entities;
 
 import java.util.Date;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -23,12 +27,17 @@ import lombok.ToString;
 public class User {
 	@Id
 	private String id;
+	@NotBlank(message = "Full Name can not be blank")
+	@Size(min = 3 , message = "Full Name should contain atleast 3 character")
 	private String fullName;
+	@NotNull(message = "email address required")
+	@Email(message = "invalid email address")
 	private String email;
 	private String department;
 	private String designation;
 	private String bio;
 	private Date joinedDate;
+	@NotBlank(message = "Role can not be blank")
 	private Role role;
 	private String photo;
 	
