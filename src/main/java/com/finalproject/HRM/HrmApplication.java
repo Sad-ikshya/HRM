16a3200a56e5fb22dead8e95f777d85b0ec0bf41
RukeshB <rukeshbasukala@gmail.com>
2021-07-30 16:28:55 +0545

    problem fixed

--- src/main/java/com/finalproject/HRM/HrmApplication.java
+++ src/main/java/com/finalproject/HRM/HrmApplication.java
@@ -1,4 +1,4 @@
-package src.main.java.com.finalproject.HRM;
+package com.finalproject.HRM;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
