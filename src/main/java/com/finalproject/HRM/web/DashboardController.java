309090a36f49ea01e13e09d2805c1cfbd7b66af3
RukeshB <rukeshbasukala@gmail.com>
2021-08-04 18:18:42 +0545

    pagination on upcomimg holiday

--- src/main/java/com/finalproject/HRM/web/DashboardController.java
+++ src/main/java/com/finalproject/HRM/web/DashboardController.java
@@ -2,10 +2,6 @@ package com.finalproject.HRM.web;
 
 import java.math.BigInteger;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.List;
-import java.util.Locale;
-import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -13,11 +9,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.finalproject.HRM.web.holiday.dtos.HolidayDto;
+import com.finalproject.HRM.web.holiday.dtos.HolidayPaginationData;
 import com.finalproject.HRM.web.holiday.services.HolidayService;
 import com.finalproject.HRM.web.leave.responseDtos.LeaveRequestResponse;
 import com.finalproject.HRM.web.leave.services.LeaveRequestService;
@@ -32,8 +27,13 @@ public class DashboardController {
 	private LeaveRequestService leaveRequestService;
 
 	@GetMapping("/upcoming-holiday")
-	public List<HolidayDto> getUpcomingHoliday() {
-		return holidayService.getUpcomingHoliday();
+	public HolidayPaginationData getUpcomingHoliday(
+							@RequestParam(name = "pageNo", defaultValue = "0") int pageNo,
+							@RequestParam(name = "limit", defaultValue = "2") int limit,
+							@RequestParam(name = "sortBy", defaultValue = "id") String sortBy
+							) 
+	{
+		return holidayService.getUpcomingHoliday(pageNo, limit, sortBy);
 	}
 
 	@GetMapping("/date/{date}")
--- src/main/java/com/finalproject/HRM/web/holiday/repositories/HolidayRepository.java
+++ src/main/java/com/finalproject/HRM/web/holiday/repositories/HolidayRepository.java
@@ -3,10 +3,12 @@ package com.finalproject.HRM.web.holiday.repositories;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.finalproject.HRM.web.holiday.entities.Holiday;
 
 public interface HolidayRepository extends MongoRepository<Holiday, String>{
-	public List<Holiday> findByDateGreaterThan(Date date);
+	public Page<Holiday> findByDateGreaterThan(Date date,Pageable page);
 }
